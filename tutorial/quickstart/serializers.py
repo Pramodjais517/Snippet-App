0f60205ca6f049399ca9082af49b00948634d89b
Pramod-Jaiswal <pramodjaiswal517@gmail.com>
2018-11-19 22:16:42 +0530

    responses done

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        field = ('url', 'user', 'email', 'groups')
+        fields = ('url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
