f90eeb72efbd104056eb4d82cfc6f2b842ddbf49
Pramod-Jaiswal <pramodjaiswal517@gmail.com>
2018-11-27 02:05:43 +0530

    database changed to mongodb using djongo

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -75,13 +75,18 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'djongo',
+        'NAME': 'mydb',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
